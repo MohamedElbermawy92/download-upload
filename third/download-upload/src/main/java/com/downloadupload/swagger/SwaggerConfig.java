ff9ce4eaa29f0c0f345ec3c26bd31a5862ce0e34
elbermawy <mohamed.elbermawy@vodafone.com>
2023-09-12 19:03:14 +0300

    remove config

--- third/download-upload/src/main/java/com/downloadupload/swagger/SwaggerConfig.java
+++ third/download-upload/src/main/java/com/downloadupload/swagger/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.downloadupload.swagger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;

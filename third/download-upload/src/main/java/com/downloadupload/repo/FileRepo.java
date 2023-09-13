c848135fcbc55f1cfb3789c8429d47d18cb726ae
elbermawy <mohamed.elbermawy@vodafone.com>
2023-09-14 00:59:26 +0300

    fix issue

--- third/download-upload/src/main/java/com/downloadupload/repo/FileRepo.java
+++
@@ -1,9 +0,0 @@
-package com.downloadupload.repo;
-
-import com.downloadupload.model.Files;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepo extends JpaRepository<Files,Integer> {
-}
--- third/download-upload/src/main/java/com/downloadupload/repo/PermissionGroupRepo.java
+++
@@ -1,9 +0,0 @@
-package com.downloadupload.repo;
-
-import com.downloadupload.model.Permissiongroups;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionGroupRepo extends JpaRepository<Permissiongroups,Integer> {
-}
